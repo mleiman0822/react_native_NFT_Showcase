1ed806b98134a9b05a75db17d4f741485ac6ab02
Matthew T Leiman <matthewtleiman@gmail.com>
2022-04-12 12:41:31 -0600

    last commit

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,10 +7,6 @@ const Download = () => {
   return (
     <div className={`${styles.section} ${styles.bgWhite}`}>
       <div className={`${styles.subSection} flex-col text-center`}>
-        <div>
-          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
-          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
-        </div>
         <button className={styles.btnPrimary}>Source Code</button>
         <div className={styles.flexCenter}>
           <img 
